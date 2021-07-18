cd1a7df6601337f039613ac6e28ea499fb859acc
Oleg <bezrukov-oa@nm.ru>
2021-07-18 20:48:00 +0300

    change functions on server to classes

--- server/server.py
+++ server/server.py
@@ -3,94 +3,85 @@ import socket
 import pickle
 
 
-def create_socket(addr) -> socket.socket:
-    s = socket.socket()
-    s.bind(addr)
-    s.listen(5)
-    s.settimeout(1)
-    return s
+class ChatServer():
+    def __init__(self, addr):
+        self.socket = socket.socket()
+        self.socket.bind(addr)
+        self.socket.listen(5)
+        self.socket.settimeout(1)
+        self.clients = []
+        self.groups = {}
+        self.logins = {}
 
+    def handle_data(self, data, client):
+        print(data)
+        if data['action'] == 'msg':
+            if data['to'][0] == '#':
+                for cl in self.groups[data['to']]:
+                    data['flag'] = 'сообщение из группы'
+                    cl.send(pickle.dumps(data))
+            else:
+                self.logins[data['to']].send(pickle.dumps(data))
+        elif data['action'] == 'presence':
+            self.logins[data['user']] = client
+            print(self.logins.keys())
+        elif data['action'] == 'quit':
+            client.close()
+            print(f'Клиент {client.fileno()}  отключился')
+            self.clients.remove(client)
+            return None
+        elif data['action'] == 'join':
+            if not self.groups.get(data['room']):
+                self.groups[data['room']] = []
+            self.groups[data['room']].append(client)
+            print('Клиент присоединился к группе')
 
-def handle_data(data, client, clients):
-    print(data)
-    if data['action'] == 'msg':
-        if data['to'][0] == '#':
-            for cl in groups[data['to']]:
-                data['flag']='сообщение из группы'
-                cl.send(pickle.dumps(data))
-        else:
-            logins[data['to']].send(pickle.dumps(data))
-    elif data['action'] == 'presence':
-        logins[data['user']] = client
-        print(logins.keys())
-    elif data['action'] == 'quit':
-        client.close()
-        print(f'Клиент {client.fileno()}  отключился')
-        clients.remove(client)
-        return None
-    elif data['action'] == 'join':
-        if not groups.get(data['room']):
-            groups[data['room']] = []
-        groups[data['room']].append(client)
-        print('Клиент присоединился к группе')
-
-
-    return pickle.dumps(data)
-
-
-def read(r, clients):
-    responses = {}
-    for client in r:
-        try:
-            data = client.recv(1024)
-            if not data:
-                raise Exception
-        except:
-            print(f'Клиент {client.fileno()} {client.getpeername()} отключился')
-            clients.remove(client)
-        else:
-            responses[client] = handle_data(pickle.loads(data), client, clients)
-    return responses
-
-
-def send(requests, w, clients):
-    for sock in requests:
-        for client in w:
+    def read(self, r):
+        responses = {}
+        for client in r:
             try:
-                client.send(requests[sock])
-            except Exception as e:
-                print(e)
+                data = client.recv(1024)
+                if not data:
+                    raise Exception
+            except:
+                print(f'Клиент {client.fileno()} {client.getpeername()} отключился')
+                self.clients.remove(client)
+            else:
+                responses[client] = self.handle_data(pickle.loads(data), client)
+        return responses
 
+    def send(self, requests, w, clients):
+        for sock in requests:
+            for client in w:
+                try:
+                    client.send(requests[sock])
+                except Exception as e:
+                    print(e)
 
-
-
-def main_loop(addr):
-    s = create_socket(addr)
-
-    clients = []
-    while True:
-        try:
-            client, addr = s.accept()
-        except OSError as e:
-            pass
-        else:
-            print(f'Получен запрос от клиента {str(addr)}')
-            clients.append(client)
-        finally:
-            wait = 1
-            r = []
-            w = []
+    def main_loop(self):
+        while True:
             try:
-                r, w, e = select.select(clients, clients, [], wait)
-            except Exception as e:
-                print(f'Исключение {e}')
-            requests = read(r, clients)
-            # if requests:
-            #     send(requests, w, clients)
+                client, addr = self.socket.accept()
+            except OSError as e:
+                pass
+            else:
+                print(f'Получен запрос от клиента {str(addr)}')
+                self.clients.append(client)
+            finally:
+                wait = 1
+                r = []
+                w = []
+                try:
+                    r, w, e = select.select(self.clients, self.clients, [], wait)
+                except Exception as e:
+                    print(f'Исключение {e}')
+                requests = self.read(r)
+                # if requests:
+                #     send(requests, w, clients)
+
 
 
 if __name__ == '__main__':
-    logins = {}
-    groups = {}
-    addr = ('', 8888)
-    main_loop(addr)
+    addr = ('', 8887)
+    serv = ChatServer(addr)
+    serv.main_loop()
--- task_4.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-if __name__ == '__main__':
-    server = subprocess.Popen('python3 server/server.py', shell=True)
-    n = int(input('Введите количество клиентов мессенджера'))
-    for i in range(n):
-        client = subprocess.Popen('python3 client/client.py', shell=True)
